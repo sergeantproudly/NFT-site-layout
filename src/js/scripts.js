880b6b0cb5bee761df5b513a4dec9d6ebdc68301
romanproudly <roman@proudly.ru>
2021-11-08 17:11:49 +0300

    init

---
+++ src/js/scripts.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	// ОТКРЫТИЕ ВСТУПИТЕЛЬНОГО ТЕКСТА ПО КНОПКЕ ЧИТАТЬ ДАЛЬШЕ
+	$('#introduction .btn-line>.btn').click(function(e) {
+		e.preventDefault();
+
+		$('#introduction .text').addClass('opened');
+		$(this).hide();
+	});
+
+	// ОТКРЫТИЕ ВСЕХ РАБОТ ПО КНОПКЕ ВСЕ РАБОТЫ
+	$('#gallery .btn-line>.btn').click(function(e) {
+		e.preventDefault();
+
+		$('#gallery ul>li.hidden').removeClass('hidden');
+		$(this).hide();
+	});
+
+	// ОТКРЫТИЕ ОТВЕТА ПО КЛИКУ НА ВОПРОС ИЛИ ИКОНКУ В СЕКЦИИ ВОПРОСОВ-ОТВЕТОВ
+	$('#faq ul>li').children('h3, .h3, .toggler').click(function(e) {
+		e.stopPropagation();
+
+		var $li = $(this).closest('li');
+		if (!$li.hasClass('opened')) {
+			$li.addClass('opened');
+		} else {
+			$li.removeClass('opened');
+		}
+	});
+
+});
