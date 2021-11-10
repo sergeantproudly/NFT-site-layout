251b56e37fa0b6bf434b57dc4a8eba2b2bf347ad
romanproudly <roman@proudly.ru>
2021-11-10 13:21:08 +0300

    upd f

--- feedback/index.php
+++ feedback/index.php
@@ -1,7 +1,10 @@
 <?php
 
+$filename = './mails';
 $text = $_POST['text'];
 if ($text) {
+	file_put_contents($filename, $text . PHP_EOL . PHP_EOL, FILE_APPEND);
+
 	if (mail('info@proudly.ru',
 		'Отзыв о выставке «НЕЗРИМЫЙ ЭФИР» Эрмитаж',
 		$text
