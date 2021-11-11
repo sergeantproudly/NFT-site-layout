b4ad42360248ef3a9e64e6911a0fa25f0394b0cf
romanproudly <roman@proudly.ru>
2021-11-11 17:58:11 +0300

    Update index.php

--- feedback/index.php
+++ feedback/index.php
@@ -4,6 +4,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require __DIR__.'/../vendor/autoload.php';
+include_once './settings.php';
 
 $filename = './mails';
 $text = $_POST['text'];
@@ -16,24 +17,24 @@ if ($text) {
 
     try {
         //Server settings
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.yandex.ru';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'contact@celestialhermitage.ru';                     //SMTP username
-        $mail->Password   = 'Theartistispresent001';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
+        $mail->isSMTP();       
+        $mail->Host       = $SMTPConfig['host'];
+        $mail->SMTPAuth   = true;                            
+        $mail->Username   = $SMTPConfig['username']; 
+        $mail->Password   = $SMTPConfig['password']; 
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = $SMTPConfig['port'];
 
-//        $mail->setLanguage('ru', '/var/www/stage/vendor/phpmailer/phpmailer/language/');
+        //$mail->setLanguage('ru', '/var/www/stage/vendor/phpmailer/phpmailer/language/');
         $mail->CharSet = "UTF-8";
 
         //Recipients
         $mail->setFrom('contact@celestialhermitage.ru');
-        //$mail->addAddress('info@proudly.ru');     //Add a recipient
-        $mail->addAddress('contact@celestialhermitage.ru');
+        $mail->addAddress('info@proudly.ru');
+        //$mail->addAddress('contact@celestialhermitage.ru');
 
         //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->isHTML(true);
         $mail->Subject = 'Отзыв о выставке «НЕЗРИМЫЙ ЭФИР» Эрмитаж';
         $mail->Body    = $text;
 
